cf3c3ca87847f6274a1d15615c45ebeed8aa5d22
Helena Martins <lenatinsjs@gmail.com>
2021-04-14 13:01:59 -0300

    setting languages

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,30 +35,32 @@ $(document).on('ready', function() {
  });
 
 
-// function changeLang(language) {
-//   const ptBR = {
-//     frase: "Meu texto em PT-BR",
-//     titleProjects: "Portfólio",
-//     titleCertificates: "Certificados & Cursos"
-//   }
+function changeLang(language) {
+  const ptBR = {
+    frase: "Olá! Meu nome é Helena Martins, sou uma desenvolvedora frontend junior brasileira. No momento, façocursos na Alura, na Faculdade iv2, além de outros, para desenvolver minhas habilidades. Minhas habilidades proncipais são HTML5, CSS3 e Javascript. Também sou fluente em inglês.",
+    titleProjects: "Portfólio",
+    titleCertificates: "Certificados & Cursos"
+  }
 
-//   var x = 0;
-
-//   console.log(ptBR.frase)
   
-//   const eng = {
-//     frase: "Meu texto em ENG",
-//     titleProjects: "Portfplio",
-//     titleCertificates: "Certificates & Courses"
-//   }
+  const eng = {
+    frase: "Hello! My name's Helena Martins, and I'm a Brazilian front-end developer junior. I am taking couses from Alura, Faculdade iv2 and other institutions in order to develop my skills. My main skills are HTML5, CSS3 and Javascript. I'm also a fluent English speaker.",
+    titleProjects: "Portfolio",
+    titleCertificates: "Certificates & Courses"
+  }
 
-//   var texto = $("#aboutme");
-//   var titlePort = $("#idTitlePort");
-//   var titleCert = $("#idTitleCert");
+  const texto = document.querySelector("#aboutme");
+  const titlePortfolio = document.querySelector("#portfolioTitle");
+  const titleCert = document.querySelector("#cec");
 
-//   if(language === 'en' ) {
-    
-//   } else {
+  if(language === 'en' ) {
+    texto.textContent = eng.frase;
+    titlePortfolio.textContent = eng.titleProjects;
+    titleCert.textContent = eng.titleCertificates;
+  } else {
+    texto.textContent = ptBR.frase;
+    titlePortfolio.textContent = ptBR.titleProjects;
+    titleCert.textContent = ptBR.titleCertificates;
 
-//   }
-// }
+  }
+}
